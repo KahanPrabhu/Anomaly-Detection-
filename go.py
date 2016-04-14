4484e9bed65634848c85ec879d446716b10cf11f
Chandler Gonzales <jcgsville@gmail.com>
2016-04-13 20:14:11 -0400

    Testing 1/6 of the data on google cloud

--- go.py
+++ go.py
@@ -1,5 +1,8 @@
 from main import main
 
+"""
 for x in xrange(1, 73):
     print "Running for latitude", str(x)
     main(x - 1, x, 0, 144, '/home/csc422/topcsvdata/', x)
+"""
+main(0, 47, 0, 35, '/topcsvdata/', 0)
